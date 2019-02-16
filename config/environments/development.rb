3ab81c5ca5595e5a82bb3d10e4758435159077f7
Carolyn Rosemary Winifred Cole <carolynrosemarywinifredcole@Carolyns-Air.fios-router.home>
2019-02-15 19:22:49 -0500

    fixed database issues

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,8 +39,8 @@ Rails.application.configure do
 config.action_mailer.smtp_settings = {
  :address              => "smtp.gmail.com",
  :port                 => 587,
- :user_name            => #mygmail,
- :password             => #mygmailpwd,
+ :user_name            => "rjeanpierre90@gmail.com",
+ :password             => "Crusader1!",
  :authentication       => "plain",
 :enable_starttls_auto => true
 }
